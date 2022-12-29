c15f9d41ec37e9585be44f9b3846cc631197781a
Jiri <63856816+cahaj@users.noreply.github.com>
2022-12-29 21:17:40 +0100

    lists

---
+++ sort.py
@@ -0,0 +1,18 @@
+import json
+with open("oglist.json", "r", encoding="utf-8") as f:
+    oglist = json.load(f)
+    oglist = [x.lower() for x in oglist]
+    print(oglist)
+
+def split_list(lst, char):
+    return filter(lambda x: x[0] == char, lst)
+
+def sort():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    for i in letters:
+        words = list(split_list(oglist, i))
+        with open(f"oglist_{i}.json", "w", encoding="utf-8") as f:
+            json.dump(words, f)
+
+if __name__ == '__main__':
+    sort()
